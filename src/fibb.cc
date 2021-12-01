b610d7217ea2ad395ba91da6ee96b33c5f920a74
David Poole <dpoole34@gatech.edu>
2021-12-01 17:34:52 -0500

    Clang-format files

--- src/fibb.cc
+++ src/fibb.cc
@@ -2,23 +2,24 @@
 
 #include <iostream>
 #include <string>
-#include <unordered_map> 
+#include <unordered_map>
 
 int main(int argc, char* argv[]) {
-  long long int n = atoll(argv[1]);	
+  long long int n = atoll(argv[1]);
 
-  std::unordered_map<Algorithm, std::string> algorithms; 
-  //algorithms[Algorithm::CXX] = "CXX";
+  std::unordered_map<Algorithm, std::string> algorithms;
+  // algorithms[Algorithm::CXX] = "CXX";
   algorithms[Algorithm::OPENMP] = "OPENMP";
   algorithms[Algorithm::SERIAL] = "SERIAL";
-  
+
   for (auto algorithm : algorithms) {
-    RecursiveFibonacci fibb_eng(n-1, algorithm.first);
+    RecursiveFibonacci fibb_eng(n - 1, algorithm.first);
     fibb_eng.run();
-   
+
     std::cout << "The " << n << "th number of the Fibonacci series using the "
-      << algorithm.second << " algorithm is: " << fibb_eng.val() << std::endl;
-    std::cout << "The time it took to complete algorithm " << algorithm.second 
-      << " is: " << fibb_eng.elapsed()/1000.0 << " s." << std::endl;
+              << algorithm.second << " algorithm is: " << fibb_eng.val()
+              << std::endl;
+    std::cout << "The time it took to complete algorithm " << algorithm.second
+              << " is: " << fibb_eng.elapsed() / 1000.0 << " s." << std::endl;
   }
 }
--- src/fibb.h
+++ src/fibb.h
@@ -1,46 +1,49 @@
+#include <unistd.h>
 #include <chrono>
 #include <future>
 #include <memory>
 #include <thread>
-#include <unistd.h>
 
 #include "algs.h"
 
 class RecursiveFibonacci {
-  long long int n_; //fibonacci number of calculate
-  long long int val_; //value of n_'th fibonacci number
-  double elapsed_; //time elapsed during calculation  
+  long long int n_;    // fibonacci number of calculate
+  long long int val_;  // value of n_'th fibonacci number
+  double elapsed_;     // time elapsed during calculation
 
-  std::unique_ptr<RecursiveFibonacciAlgorithm> algorithm_; //pointer to algorithm functor
-  
-public:
+  std::unique_ptr<RecursiveFibonacciAlgorithm>
+      algorithm_;  // pointer to algorithm functor
+
+ public:
   //-- constructors and destructors --//
-  RecursiveFibonacci(long long int n, Algorithm algorithm=Algorithm::CXX) 
-  : n_(n), val_(0), elapsed_(0.0), algorithm_(nullptr) 
-  {
-    switch(algorithm) {
-      case Algorithm::CXX: 
-    	algorithm_ = std::unique_ptr<CxxAlgorithm>(new CxxAlgorithm());  
+  RecursiveFibonacci(long long int n, Algorithm algorithm = Algorithm::CXX)
+      : n_(n), val_(0), elapsed_(0.0), algorithm_(nullptr) {
+    switch (algorithm) {
+      case Algorithm::CXX:
+        algorithm_ = std::unique_ptr<CxxAlgorithm>(new CxxAlgorithm());
         break;
       case Algorithm::OPENMP:
-    	algorithm_ = std::unique_ptr<OpenMPAlgorithm>(new OpenMPAlgorithm());  
+        algorithm_ = std::unique_ptr<OpenMPAlgorithm>(new OpenMPAlgorithm());
         break;
-      case Algorithm::SERIAL: 
-    	algorithm_ = std::unique_ptr<SerialAlgorithm>(new SerialAlgorithm());  
+      case Algorithm::SERIAL:
+        algorithm_ = std::unique_ptr<SerialAlgorithm>(new SerialAlgorithm());
         break;
-      default: throw std::invalid_argument("Specified algorithm does not exist!"); break;
+      default:
+        throw std::invalid_argument("Specified algorithm does not exist!");
+        break;
     }
   }
 
   /*
-  RecursiveFibonacci(long long int n, int omp_num_threads) 
-  : n_(n), val_(0), elapsed_(0.0) 
+  RecursiveFibonacci(long long int n, int omp_num_threads)
+  : n_(n), val_(0), elapsed_(0.0)
   {
-    algorithm_ = std::make_unique<RecursiveFibonacciAlgorithm>(new OpenMPAlgorithm(omp_num_threads));  
+    algorithm_ = std::make_unique<RecursiveFibonacciAlgorithm>(new
+  OpenMPAlgorithm(omp_num_threads));
   }
   */
 
-  ~RecursiveFibonacci() { };
+  ~RecursiveFibonacci(){};
 
   //-- setters and getters --//
   void set_n(long long int n) { n_ = n; }
@@ -52,18 +55,19 @@ public:
 
   long long int elapsed() { return elapsed_; }
   const long long int elapsed() const { return elapsed_; }
- 
+
   Algorithm algorithm() { return algorithm_->algorithm(); }
   const Algorithm algorithm() const { return algorithm_->algorithm(); }
 
   //-- compute nth fibonacci number --//
   void run() {
     auto start_time = std::chrono::system_clock::now();
-    val_ = algorithm_->compute_fibb(n_); 
+    val_ = algorithm_->compute_fibb(n_);
     auto end_time = std::chrono::system_clock::now();
-    
-    elapsed_ = std::chrono::duration_cast<std::chrono::milliseconds>(end_time 
-      - start_time).count();
+
+    elapsed_ = std::chrono::duration_cast<std::chrono::milliseconds>(end_time -
+                                                                     start_time)
+                   .count();
   }
 };
 
